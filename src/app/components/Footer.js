80ba671fd80c1cda203bf5d299dc99124f901adc
Hunter-Reboul <hunter_reboul@hotmail.ca>
2025-02-13 16:52:21 -0700

    Created Footer Placeholder, started working on Intro Page Layout

---
+++ src/app/components/Footer.js
@@ -0,0 +1,9 @@
+import styles from "./components.module.css";
+
+export default function Footer() {
+    return (
+        <div className={styles.box}>
+            <p className={styles.footerText}>Footer Placeholder</p>
+        </div>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,21 @@ export default function Home() {
     return (
         <div className={styles.page}>
             <h1 className={styles.title}>Hunter Reboul - Software Engineer</h1>
-            <Image className={styles.profile}
-                src="/Hunter.png"
-                width={355}
-                height={418}
-                alt="An image of Hunter Reboul smiling at the camera while holding up a Varjo XR-3 headset."
-            />
-            <p className={styles.pageText}>Alright. Time to get to work.</p>
+            <div className={styles.introContainer}>
+                <p className={styles.pageText}>
+                    A Junior Software Engineer currently based in Calgary, AB.<br /><br />
+                    If you are seeking someone with a broad technical skillset, the ability to both
+                    problem-solve on their own and collaborate efficiently with a team, and a motivated
+                    and rapid learning, then look no further.<br /><br />
+                    I even taught myself React and Next.js over the course of a month just to create this
+                    site from scratch.<br /><br />
+                    Want to learn more? Click any of the links below!
+                </p>
+                <img className={styles.profile}
+                    src="/Hunter.png"
+                    alt="An image of Hunter Reboul smiling at the camera while holding up a Varjo XR-3 headset."
+                />
+            </div>
         </div>
     );
 }

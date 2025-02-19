9169f3e2dc516334e4876ec6d5560be2b8816cff
Hunter-Reboul <hunter_reboul@hotmail.ca>
2025-02-19 12:35:54 -0700

    Intro text and profile picture change size based on page width

--- src/app/page.js
+++ src/app/page.js
@@ -7,18 +7,19 @@ export default function Home() {
             <h1 className={styles.title}>Hunter Reboul - Software Engineer</h1>
             <div className={styles.introContainer}>
                 <p className={styles.pageText}>
-                    A Junior Software Engineer currently based in Calgary, AB.<br /><br />
+                    I'm a Junior Software Engineer currently based in Calgary, AB.<br /><br />
                     If you are seeking someone with a broad technical skillset, the ability to both
                     problem-solve on their own and collaborate efficiently with a team, and a motivated
                     and rapid learning, then look no further.<br /><br />
                     I even taught myself React and Next.js over the course of a month just to create this
                     site from scratch.<br /><br />
-                    Want to learn more? Click any of the links below!
                 </p>
                 <img className={styles.profile}
                     src="/Hunter.png"
                     alt="An image of Hunter Reboul smiling at the camera while holding up a Varjo XR-3 headset."
                 />
+            </div>
+            <div className={styles.skillsContainer}>
             </div>
         </div>
     );

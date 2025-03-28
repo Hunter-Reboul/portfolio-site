9d7f6f311b864a90052a1c8b3c4a3ec192467392
Hunter-Reboul <hunter_reboul@hotmail.ca>
2025-03-28 11:52:35 -0600

    Fixed Footer to be more compact

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -4,13 +4,13 @@ export default function Footer() {
     return (
         <div className={styles.footerBox}>
             <div className={styles.footerContainer}>
-                <p className={styles.contactText}><b>Contact Me</b></p>
-                <p className={styles.emailText}>hunter_reboul@hotmail.ca</p>
-                <p className={styles.phoneText}>(587) 624-4332</p>
-                <p className={styles.socialsText}><b>Socials</b></p>
+                <div className={styles.contactText}><b>Contact Me</b></div>
+                <div className={styles.emailText}>hunter_reboul@hotmail.ca</div>
+                <div className={styles.phoneText}>(587) 624-4332</div>
+                <div className={styles.socialsText}><b>Socials</b></div>
                 <a className={styles.linkedinText} href="https://www.linkedin.com/in/hunter-reboul/">LinkedIn</a>
                 <a className={styles.githubText} href="https://github.com/Hunter-Reboul">GitHub</a>
-                <p className={styles.copyrightText}>&copy; 2025 Hunter Reboul</p>
+                <div className={styles.copyrightText}>&copy; 2025 Hunter Reboul</div>
             </div>
         </div>
     );
